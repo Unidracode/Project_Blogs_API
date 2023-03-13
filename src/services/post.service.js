949e85ffc762aef13c3400eec49bc1157c1939c6
Unidracode <unidragorn@gmail.com>
2023-03-13 19:36:32 -0300

    corrige post

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,4 +1,3 @@
-const { User } = require('../models');
 const { BlogPost } = require('../models');
 const { Category } = require('../models');
 const { PostCategory } = require('../models');
@@ -26,35 +25,6 @@ const createPost = async ({ id, title, content, categoryIds }) => {
       published: post.published.val };
 };
 
-const filterPosts = (posts) => posts.map((post) => ({
-  id: post.id,
-  title: post.title,
-  content: post.content,
-  userId: post.userId,
-  published: post.published,
-  updated: post.updated,
-  user: {
-    id: post.user.id,
-    displayName: post.user.displayName,
-    email: post.user.email,
-    image: post.user.image,
-  },
-  categories: post.categories,
-}));
-
-const getPostById = async (postId) => {
-  const post = await BlogPost.findByPk(postId, {
-      include: [
-          { model: User, as: 'user' },
-          { model: Category, as: 'categories', through: { attributes: [] } },
-      ],
-  });
-  if (!post) throw new Error('Post does not exist');
-  const [postFiltered] = filterPosts([post]);
-  return postFiltered;
-};
-
 module.exports = {
   createPost,
-  getPostById,
 };
