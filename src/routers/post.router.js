282bbc85335bf7d0cde58e817b541e114454f527
Unidracode <unidragorn@gmail.com>
2023-03-13 21:25:01 -0300

    corrige e adiciona post

--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -3,16 +3,51 @@ const { postController } = require('../controllers');
 const validatePostFields = require('../middlewares/validatePostFields');
 const errorHandler = require('../middlewares/postErrorHandler');
 const validateJWT = require('../middlewares/validateJWT');
+const validateUpdatePostFields = require('../middlewares/validateUpdatePostFields');
+const validateUserPost = require('../middlewares/validateUserPost');
 
 require('express-async-errors');
 
 const router = express.Router();
+
+router.get(
+  '/search',
+  validateJWT,
+  postController.getSearchedPosts,
+);
 
 router.post(
   '/',
   validateJWT,
   validatePostFields,
   postController.createPost,
+);
+
+router.get(
+  '/',
+  validateJWT,
+  postController.getPosts,
+);
+
+router.get(
+  '/:id',
+  validateJWT,
+  postController.getPostById,
+);
+
+router.put(
+  '/:id',
+  validateJWT,
+  validateUpdatePostFields,
+  validateUserPost,
+  postController.updatePostById,
+);
+
+router.delete(
+  '/:id',
+  validateJWT,
+  validateUserPost,
+  postController.deletePostById,
 );
 
 router.use(errorHandler.createPost);
